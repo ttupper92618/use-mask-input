f3afba06e80f63d74a56d60c616316101311f108
Eduardo Borges <euduardoborges@gmail.com>
2020-08-14 11:28:38 -0300

    Updates

---
+++ precache-manifest.b22c8684d0e257a5737045ac8eca4cf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a76ecce127e6f37db8ec479b2901228",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62d07afdb57db9c6ba8f",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "9462f61c5bbed9dd9a5b",
+    "url": "./static/js/2.a10b05a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a10b05a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d07afdb57db9c6ba8f",
+    "url": "./static/js/main.25dc3074.chunk.js"
+  },
+  {
+    "revision": "acf3ec290eec446f86e0",
+    "url": "./static/js/runtime-main.e2130113.js"
+  }
+]);
